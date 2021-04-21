a16b2326c3a95f90b6695a7aec7c3c04c7a4a83d
Michael Ushakov <um.nix.user@gmail.com>
2021-04-21 16:02:58 +0500

    initial model

--- gorm/db_transactions_test.go
+++ gorm/db_transactions_test.go
@@ -1,1 +1,33 @@
 package gorm
+
+import (
+	"github.com/jinzhu/gorm"
+	g "gorm.io/gorm"
+	"testing"
+)
+
+type user struct {
+	g.Model
+	UserName string `gorm:"type:varchar(128);not null;"`
+	PasswordHash string `gorm:"type:varchar(255);not null;"`
+	ProfileId uint
+	Profile profile `gorm:"foreignkey:ProfileID;"`
+	Roles []roles `gorm:"many2many:user_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+type profile struct {
+	g.Model
+	Name string `gorm:"type:varchar(128);not null;"`
+}
+
+type roles struct {
+	g.Model
+	Name string `gorm:"type:varchar(128);not null;"`
+}
+
+func TestModelWithMultipleTransactions(t *testing.T) {
+}
+
+func prepareDatabase(db *g.DB) {
+
+}
