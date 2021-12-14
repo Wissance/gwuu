a370cc571d6e38c7918e688544abd16dea8f744e
Michael Ushakov <um.nix.user@gmail.com>
2021-12-14 19:29:59 +0500

    checkers (strings, unordered works fine)

--- testingutils/checkers.go
+++ testingutils/checkers.go
@@ -6,26 +6,51 @@ import (
 	"testing"
 )
 
+const (
+	ExpectedOrActualBothNotNil = "One of arrays (expected, actual) is nil other is not nil"
+	ArraysLengthAreNotSame = "Arrays length are not same"
+	ItemNotFound = "Expected array item: \"{0}\" at index: \"{1}\" was not found in actual array"
+)
+
 // CheckStrings
-/*  This function allow to compare two arrays of Strings with order and without it
+/*  This function allow to compare two arrays of Strings with order and without it and with asserts (assertErr is true) and without
  *  Parameters:
  *     - t is Test State, because we provide functions to check equality in tests
  *     - expected - one array of strings
  *     - actual - another array of strings
  *     - checkOrder - parameter that is responsible for check data with respect to order of arrays items
+ *     - assertErr - assert if True otherwise just use a result
  *  Functions return nothing and asserts if arrays are not equals
  *  Returns true if there is no assert fail, otherwise - false
  */
-func CheckStrings(t *testing.T, expected []string, actual []string, checkOrder bool) bool {
+func CheckStrings(t *testing.T, expected []string, actual []string, checkOrder bool, assertErr bool) (bool, string) {
 	if expected == nil || actual == nil{
-		assert.Nil(t, expected, "Checking that expected is nil")
-		assert.Nil(t, actual, "Checking that actual is nil")
+		nilArraysCheck := expected == nil && actual == nil
+		if assertErr {
+			assert.Nil(t, expected, "Checking that expected is nil")
+			assert.Nil(t, actual, "Checking that actual is nil")
+		}
+		if nilArraysCheck {
+			return true, ""
+		}
+		return false, ExpectedOrActualBothNotNil
 	}
-    assert.Equal(t, len(expected), len(actual), "Checking that arrays length are equals")
+	if len(expected) != len(actual) {
+		if assertErr {
+			assert.Equal(t, len(expected), len(actual), "Checking that arrays length are equals")
+		}
+		return false, ArraysLengthAreNotSame
+	}
+
+	itemCheck := true
+	// created empty array that will be populated if we found items
 	usedObjects := make([]int, 0)
 	for i, eItem := range expected {
 		if checkOrder {
-			assert.Equal(t, expected[i], actual[i])
+			itemCheck = itemCheck && (expected[i] == actual[i])
+			if assertErr {
+				assert.Equal(t, expected[i], actual[i])
+			}
 		} else {
 			unOrderedCheck := false
 			for j, aItem := range actual {
@@ -38,10 +63,16 @@ func CheckStrings(t *testing.T, expected []string, actual []string, checkOrder b
 					}
 				}
 			}
-			assert.True(t, unOrderedCheck, stringFormatter.Format("Checking object \"{0}\" exists in actual array", eItem))
+			itemCheck = itemCheck && unOrderedCheck
+			if assertErr {
+				assert.True(t, unOrderedCheck, stringFormatter.Format("Checking object \"{0}\" exists in actual array", eItem))
+			}
+		}
+		if !itemCheck {
+			return false, stringFormatter.Format(ItemNotFound, eItem, i)
 		}
 	}
-	return !t.Failed()
+	return true, ""
 }
 
 // CheckIntegers
--- testingutils/checkers_test.go
+++ testingutils/checkers_test.go
@@ -8,33 +8,36 @@ import (
 func TestCheckStringsSuccessfulWithoutOrder(t *testing.T) {
 	arr1 := []string{"", ""}
 	arr2 := []string{"", ""}
-	checkResult := CheckStrings(t, arr1, arr2, false)
+	checkResult, _ := CheckStrings(t, arr1, arr2, false, true)
 	assert.True(t, checkResult)
 
 	arr1 = []string{"English", "Русский"}
 	arr2 = []string{"English", "Русский"}
-	checkResult = CheckStrings(t, arr1, arr2, false)
+	checkResult, _ = CheckStrings(t, arr1, arr2, false, true)
 	assert.True(t, checkResult)
 
 	arr1 = []string{"TextOne", "ТекстДва", "TextThree", "TextFore"}
 	arr2 = []string{"TextFore", "ТекстДва", "TextOne", "TextThree"}
-	checkResult = CheckStrings(t, arr1, arr2, false)
+	checkResult, _ = CheckStrings(t, arr1, arr2, false, true)
 	assert.True(t, checkResult)
 }
 
 func TestCheckStringsFailsWithoutOrder(t *testing.T) {
 	arr1 := []string{"", ""}
 	arr2 := []string{" ", " "}
-	checkResult := CheckStrings(t, arr1, arr2, false)
+	checkResult, msg := CheckStrings(t, arr1, arr2, false, false)
 	assert.False(t, checkResult)
+	assert.NotEmpty(t, msg)
 
 	arr1 = []string{"english", "Русский"}
 	arr2 = []string{"English", "русский"}
-	checkResult = CheckStrings(t, arr1, arr2, false)
-	assert.True(t, checkResult)
+	checkResult, msg = CheckStrings(t, arr1, arr2, false, false)
+	assert.False(t, checkResult)
+	assert.NotEmpty(t, msg)
 
 	arr1 = []string{"TextOne", "ТекстДва", "TextThree", "TextFore"}
 	arr2 = []string{"TextFore", "ТекстДва", "TextOne"}
-	checkResult = CheckStrings(t, arr1, arr2, false)
-	assert.True(t, checkResult)
+	checkResult, msg = CheckStrings(t, arr1, arr2, false, false)
+	assert.False(t, checkResult)
+	assert.NotEmpty(t, msg)
 }
