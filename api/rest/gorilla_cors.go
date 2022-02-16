bab2ca0f13a1f9d9aced81b97413f76dd50b5031
Michael Ushakov <um.nix.user@gmail.com>
2022-02-16 20:03:53 +0500

    moving on

--- api/rest/gorilla_cors.go
+++ api/rest/gorilla_cors.go
@@ -2,14 +2,18 @@ package rest
 
 import (
 	m "github.com/gorilla/mux"
+	"github.com/wissance/stringFormatter"
 	"net/http"
 )
 
 const (
+	// AccessControlAllowMethodsHeader - CORS Header that says what HTTP Methods are allowed to specific endpoint
 	AccessControlAllowMethodsHeader = "Access-Control-Allow-Methods"
 	AccessControlAllowOriginHeader = "Access-Control-Allow-Origin"
 	AccessControlAllowHeadersHeader = "Access-Control-Allow-Headers"
+	// Value that allow all headers
 	AllowAllHeaderValues = "*"
+	optionsRouteSuffix = "opts"
 )
 
 /*type WebApi interface {
@@ -17,6 +21,8 @@ const (
 }*/
 
 type WebApiHandler struct {
+	corsConfig map[string][]string
+	AllowCors bool
 	Origin string
 	Router *m.Router
 }
@@ -28,7 +34,7 @@ type WebApiHandler struct {
  * Returns nothing
  */
 func EnableCorsAllOrigin(respWriter *http.ResponseWriter) {
-	EnableCors(respWriter, AllowAllHeaderValues)
+	EnableCors(respWriter, AllowAllHeaderValues, AllowAllHeaderValues)
 }
 
 // EnableCors
@@ -36,15 +42,41 @@ func EnableCorsAllOrigin(respWriter *http.ResponseWriter) {
  * Parameters:
  *     - respWriter - gorilla/mux response writer
  *     - origin - domain i.e. example.com or http://127.0.0.1:3000
+ *     - methods - allowed methods i.e. GET, POST, OPTIONS
  * Returns nothing
  */
-func EnableCors(respWriter *http.ResponseWriter, origin string) {
+func EnableCors(respWriter *http.ResponseWriter, origin string, methods string) {
 	(*respWriter).Header().Set(AccessControlAllowOriginHeader, origin)
-	(*respWriter).Header().Set(AccessControlAllowHeadersHeader, AllowAllHeaderValues)
+	(*respWriter).Header().Set(AccessControlAllowHeadersHeader, methods)
 }
 
-func (handler *WebApiHandler) HandleFunc(url string, f func(http.ResponseWriter, *http.Request), allowCors bool) {
+// HandleFunc
+/* This is a Proxy function that assign handler to handle specific route by url but also simultaneously it configures CORS handler.
+ * We are working here with route names for OPTIONS handler i.e. we have REST resource /api/good and 2 separate handlers for GET and POST
+ * therefore for proper CORS handle we should respond on OPTIONS /api/good with empty body and header AccessControlAllowMethodsHeader with
+ * values OPTIONS, GET, POST. Our HandleFunc allow to reduce a complexity of router config because using our HandleFunc we take service on
+ * handling OPTIONS method by our HandleFunc.
+ * Parameters:
+ * Return *Route
+ */
+func (handler *WebApiHandler) HandleFunc(url string, f func(http.ResponseWriter, *http.Request), handlerMethods ...string) *m.Route {
 	// 1. Create Route ...
-	// 2. Create Options route
-
+	route := handler.Router.HandleFunc(url, f).Methods(handlerMethods...)
+	if handler.AllowCors {
+		// 2. Create Options route
+		optionRouteName := stringFormatter.Format("{0}_{1}", url, optionsRouteSuffix)
+		optionsRoute := handler.Router.GetRoute(optionRouteName)
+		if optionsRoute == nil {
+			// there is no Route with such name, so we could easily create it and assign methods = handlerMethods + "OPTIONS"
+			// set Router
+			handler.corsConfig[optionRouteName] = []string{"OPTIONS"}
+			// combine with handlerMethods
+			// assign Handler
+		} else {
+			// route already exists, therefore we should only append missing methods
+			// append handlerMethods
+			// update RouteMethods
+		}
+	}
+	return route
 }
