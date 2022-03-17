9c7ff399ad0a04d7c446eb3bbae5a795e621615e
Michael Ushakov <um.nix.user@gmail.com>
2022-03-18 00:32:50 +0500

    seems that tests on preflight set headers correctly works fine

--- api/rest/gorilla_cors_test.go
+++ api/rest/gorilla_cors_test.go
@@ -19,33 +19,38 @@ func TestHandleFuncWithCorsWithAnyOrigin(t *testing.T) {
 
 	}, "GET")
 	// full crud
-	handler.HandleFunc("/api/user/", func(writer http.ResponseWriter, request *http.Request) {
+	userResourceRoot := "/api/user/"
+	handler.HandleFunc(userResourceRoot, func(writer http.ResponseWriter, request *http.Request) {
 
 	}, "GET")
-	handler.HandleFunc("/api/user/", func(writer http.ResponseWriter, request *http.Request) {
+	handler.HandleFunc(userResourceRoot, func(writer http.ResponseWriter, request *http.Request) {
 
 	}, "POST")
-	handler.HandleFunc("/api/user/{id:[0-9]+}/", func(writer http.ResponseWriter, request *http.Request) {
+	userResourceById := "/api/user/{id:[0-9]+}/"
+	handler.HandleFunc(userResourceById, func(writer http.ResponseWriter, request *http.Request) {
+
+	}, "GET")
+	handler.HandleFunc(userResourceById, func(writer http.ResponseWriter, request *http.Request) {
 
 	}, "PUT")
-	handler.HandleFunc("/api/user/{id:[0-9]+}/", func(writer http.ResponseWriter, request *http.Request) {
+	handler.HandleFunc(userResourceById, func(writer http.ResponseWriter, request *http.Request) {
 
 	}, "DELETE")
 
-	realmOptionRoute := stringFormatter.Format("{0}_{1}", realmResource, optionsRouteSuffix)
-	route := handler.Router.Get(realmOptionRoute)
+	realmOptionRouteName := stringFormatter.Format("{0}_{1}", realmResource, optionsRouteSuffix)
+	route := handler.Router.Get(realmOptionRouteName)
 	assert.NotNil(t, route)
-	checkOptionRouteCors(t, route,realmResource, "*", "*", "OPTIONS,GET" )
-	/*m, _ := route.Methods().GetMethods()
-	assert.Equal(t, 1, len(m))
-	assert.Equal(t, "OPTIONS", m[0])
-	request := http.Request{URL: &url.URL{Scheme: "http", Host: "127.0.0.1", Path: realmResource},
-		                Method: "OPTIONS"}
-	writer := httptest.NewRecorder()
-	route.GetHandler().ServeHTTP(writer, &request)
-	assert.Equal(t, "*", writer.Header().Get(AccessControlAllowOriginHeader))
-	assert.Equal(t, "*", writer.Header().Get(AccessControlAllowHeadersHeader))
-	assert.Equal(t, "OPTIONS,GET", writer.Header().Get(AccessControlAllowMethodsHeader))*/
+	checkOptionRouteCors(t, route, realmResource, "*", "*", "OPTIONS,GET" )
+
+	userRootOptionRouteName := stringFormatter.Format("{0}_{1}", userResourceRoot, optionsRouteSuffix)
+	route = handler.Router.Get(userRootOptionRouteName)
+	assert.NotNil(t, route)
+	checkOptionRouteCors(t, route, userResourceRoot, "*", "*", "OPTIONS,GET,POST" )
+
+	userByIdOptionRouteName := stringFormatter.Format("{0}_{1}", userResourceById, optionsRouteSuffix)
+	route = handler.Router.Get(userByIdOptionRouteName)
+	assert.NotNil(t, route)
+	checkOptionRouteCors(t, route, userResourceById, "*", "*", "OPTIONS,GET,PUT,DELETE" )
 }
 
 func checkOptionRouteCors(t *testing.T, route *mux.Route, requestPath string, allowedOrigin string, allowedHeader string, allowedMethods string) {
