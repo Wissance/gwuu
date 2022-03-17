f6317c8a5b67bedfa2fefe860d87e54c64d9e7ee
Michael Ushakov <um.nix.user@gmail.com>
2022-03-18 01:49:38 +0500

    seems that subroutes options config works too

--- api/rest/gorilla_cors_test.go
+++ api/rest/gorilla_cors_test.go
@@ -40,23 +40,64 @@ func TestHandleFuncWithCorsWithAnyOrigin(t *testing.T) {
 	realmOptionRouteName := stringFormatter.Format("{0}_{1}", realmResource, optionsRouteSuffix)
 	route := handler.Router.Get(realmOptionRouteName)
 	assert.NotNil(t, route)
-	checkOptionRouteCors(t, route, realmResource, "*", "*", "OPTIONS,GET" )
+	checkOptionRouteCors(t, route, realmResource, AnyOrigin, "*", "OPTIONS,GET" )
 
 	userRootOptionRouteName := stringFormatter.Format("{0}_{1}", userResourceRoot, optionsRouteSuffix)
 	route = handler.Router.Get(userRootOptionRouteName)
 	assert.NotNil(t, route)
-	checkOptionRouteCors(t, route, userResourceRoot, "*", "*", "OPTIONS,GET,POST" )
+	checkOptionRouteCors(t, route, userResourceRoot, AnyOrigin, "*", "OPTIONS,GET,POST" )
 
 	userByIdOptionRouteName := stringFormatter.Format("{0}_{1}", userResourceById, optionsRouteSuffix)
 	route = handler.Router.Get(userByIdOptionRouteName)
 	assert.NotNil(t, route)
-	checkOptionRouteCors(t, route, userResourceById, "*", "*", "OPTIONS,GET,PUT,DELETE" )
+	checkOptionRouteCors(t, route, userResourceById, AnyOrigin, "*", "OPTIONS,GET,PUT,DELETE" )
 }
 
 func TestHandleFuncForSubRouterAndSpecificOrigin(t *testing.T) {
 	// there is no sub router access yet ...
-	//handler := NewWebApiHandler(true, AnyOrigin)
-	//handler.Router.
+	internalSubNet := "192.168.30.0"
+	handler := NewWebApiHandler(true, internalSubNet)
+	service1Router := handler.Router.PathPrefix("service1").Subrouter()
+
+	objectResource := "/api/object/"
+	handler.HandleFunc(service1Router, objectResource, func(writer http.ResponseWriter, request *http.Request) {
+
+	}, "GET")
+
+	handler.HandleFunc(service1Router, objectResource, func(writer http.ResponseWriter, request *http.Request) {
+
+	}, "POST")
+
+	service2Router := handler.Router.PathPrefix("service2").Subrouter()
+	classRootResource := "/api/class/"
+
+	handler.HandleFunc(service2Router, classRootResource, func(writer http.ResponseWriter, request *http.Request) {
+
+	}, "GET")
+
+	handler.HandleFunc(service2Router, classRootResource, func(writer http.ResponseWriter, request *http.Request) {
+
+	}, "POST")
+
+	classByIdResource := "/api/class/{id}/"
+	handler.HandleFunc(service2Router, classByIdResource, func(writer http.ResponseWriter, request *http.Request) {
+
+	}, "DELETE")
+
+	objectOptionRouteName := stringFormatter.Format("{0}_{1}", objectResource, optionsRouteSuffix)
+	route := service1Router.Get(objectOptionRouteName)
+	assert.NotNil(t, route)
+	checkOptionRouteCors(t, route, objectResource, internalSubNet, "*", "OPTIONS,GET,POST" )
+
+	classRootOptionRouteName := stringFormatter.Format("{0}_{1}", classRootResource, optionsRouteSuffix)
+	route = service2Router.Get(classRootOptionRouteName)
+	assert.NotNil(t, route)
+	checkOptionRouteCors(t, route, classRootResource, internalSubNet, "*", "OPTIONS,GET,POST" )
+
+	classByIdOptionRouteName := stringFormatter.Format("{0}_{1}", classByIdResource, optionsRouteSuffix)
+	route = service2Router.Get(classByIdOptionRouteName)
+	assert.NotNil(t, route)
+	checkOptionRouteCors(t, route, classByIdResource, internalSubNet, "*", "OPTIONS,DELETE" )
 }
 
 func checkOptionRouteCors(t *testing.T, route *mux.Route, requestPath string, allowedOrigin string, allowedHeader string, allowedMethods string) {
